edbf3552733510893545d2910c8ae00f1d44950d
Denis Plotnikov <denereoz@gmail.com>
2025-08-25 13:29:26 +0700

    Add Serialization

---
+++ Ereoz.Abstractions/Serialization/ISerializeService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Ereoz.Abstractions.Serialization
+{
+    public interface ISerializeService<StringOrByteArray>
+    {
+        StringOrByteArray Serialize<T>(T objectForSerialization, bool formattingIndented = false);
+
+        T Deserialize<T>(StringOrByteArray serializedObject);
+
+        object Deserialize(Type targetType, StringOrByteArray serializedObject);
+    }
+}
