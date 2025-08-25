09f8d663f9e301fa7f420552bf35d4f179b396f5
Denis Plotnikov <denereoz@gmail.com>
2025-08-25 08:02:37 +0700

    Add NullLogger

---
+++ Ereoz.Abstractions/Logging/NullLogger.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Ereoz.Abstractions.Logging
+{
+    public class NullLogger : ILogger
+    {
+        public void Debug(Exception exception) { }
+
+        public void Debug(string message, params object[] args) { }
+
+        public void Debug(Exception exception, string message, params object[] args) { }
+
+        public void Error(Exception exception) { }
+
+        public void Error(string message, params object[] args) { }
+
+        public void Error(Exception exception, string message, params object[] args) { }
+
+        public void Fatal(Exception exception) { }
+
+        public void Fatal(string message, params object[] args) { }
+
+        public void Fatal(Exception exception, string message, params object[] args) { }
+
+        public void Info(Exception exception) { }
+
+        public void Info(string message, params object[] args) { }
+
+        public void Info(Exception exception, string message, params object[] args) { }
+
+        public void Trace(Exception exception) { }
+
+        public void Trace(string message, params object[] args) { }
+
+        public void Trace(Exception exception, string message, params object[] args) { }
+
+        public void Warn(Exception exception) { }
+
+        public void Warn(string message, params object[] args) { }
+
+        public void Warn(Exception exception, string message, params object[] args) { }
+    }
+}
