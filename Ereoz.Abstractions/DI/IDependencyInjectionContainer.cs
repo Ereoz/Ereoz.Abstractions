f551ada1690b78f45030bf95ff80d2b7938da8d7
Denis Plotnikov <denereoz@gmail.com>
2025-10-08 11:22:13 +0700

    Add DI

---
+++ Ereoz.Abstractions/DI/IDependencyInjectionContainer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Ereoz.Abstractions.DI
+{
+    public interface IDependencyInjectionContainer
+    {
+        IServiceInfo Register<Service>();
+        IServiceInfo Register<Service>(Func<object> factory);
+
+        IServiceInfo Register<Contract, Implementation>();       
+        IServiceInfo Register<Contract, Implementation>(Func<object> factory);
+
+        IServiceInfo Register(Type service);
+        IServiceInfo Register(Type service, Func<object> factory);
+
+        IServiceInfo Register(Type contract, Type implementation);
+        IServiceInfo Register(Type contract, Type implementation, Func<object> factory);
+
+        Service Resolve<Service>();
+        Service Resolve<Service>(string name);
+        IEnumerable<Service> ResolveAllImplementationse<Service>();
+
+        object Resolve(Type service);
+        object Resolve(Type service, string name);        
+        IEnumerable<object> ResolveAllImplementations(Type service);
+    }
+}
---
+++ Ereoz.Abstractions/DI/IServiceInfo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ereoz.Abstractions.DI
+{
+    public interface IServiceInfo
+    {
+        Type ServiceType { get; set; }
+        object Resolve();
+        void AsSingletone();
+        void AsTransient();
+    }
+}
