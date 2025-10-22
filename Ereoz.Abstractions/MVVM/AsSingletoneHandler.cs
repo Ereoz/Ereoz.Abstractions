506006d13af25c2052d6423f9f1404759251a461
Denis Plotnikov <denereoz@gmail.com>
2025-10-22 13:00:57 +0700

    Add MVVM

---
+++ Ereoz.Abstractions/MVVM/AsSingletoneHandler.cs
@@ -0,0 +1,15 @@
+﻿namespace Ereoz.Abstractions.MVVM
+{
+    public delegate void AsSingletoneHandler();
+
+    public static class AsSingletoneHandlerExtensions
+    {
+        /// <summary>
+        /// Обёртка для метода Invoke.
+        /// </summary>
+        /// <param name="handler"></param>
+        /// <param name="obj"></param>
+        public static void AsSingletone(this AsSingletoneHandler handler) =>
+            handler?.Invoke();
+    }
+}
---
+++ Ereoz.Abstractions/MVVM/INavigationManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Ereoz.Abstractions.MVVM
+{
+    public interface INavigationManager
+    {
+        event EventHandler<EventArgs> ViewChanged;
+
+        object Navigate<T>(object sender, params object[] parameters);
+        object Navigate(object sender, Type viewOrViewModelType, params object[] parameters);
+
+        AsSingletoneHandler RegisterViewWithViewModel<TView, TViewModel>();
+        AsSingletoneHandler RegisterViewWithViewModel(Type view, Type viewModel);
+
+        void AutoRegisterAllViewsWithViewModels();
+    }
+}
